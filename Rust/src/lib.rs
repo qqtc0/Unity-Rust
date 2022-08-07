f495a0ea80718b4acc1576c8810b51bf1ece3065
devin <54314240+dev11n@users.noreply.github.com>
2022-08-07 18:14:02 +0000

    Update lib.rs

--- Rust/src/lib.rs
+++ Rust/src/lib.rs
@@ -1,11 +1,10 @@
-// * Made by qqt#0001 on December 6th, 2021
 mod library {
-    // * Import the rand class
     use rand::Rng;
 
-    // * Stop mangling of function names, which is required for Unity
+    // Stop mangling of function names, which is required for DllImport (unless you somehow enjoy passing the mangled name too).
+    // Do note though that `no_mangle` will prevent generic types [ `T` ] from working.
     #[no_mangle]
-    pub extern fn generate_random(min_range: i32, max_range: i32) -> i32
+    pub extern "C" fn generate_random(min_range: i32, max_range: i32) -> i32
     {
         rand::thread_rng().gen_range(min_range..max_range)
     }
